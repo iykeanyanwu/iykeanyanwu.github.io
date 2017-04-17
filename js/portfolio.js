2a87877872b34ea12dcab8e27502b71448f23f9d
Kosy Anyanwu <kosyfrances@gmail.com>
2017-04-17 19:41:39 +0300

    Increment portfolio image data title

--- js/portfolio.js
+++ js/portfolio.js
@@ -2,13 +2,15 @@
 // renders them on the DOM.
 
 $(function () {
+    var count = 1
 
     $.ajax({
         url: "./images.json",
         dataType: "json",
         success: function(data) {
             $.each(data, function(key, img) {
-                $(".portfolio-images").append('<div class="col-sm-4"><div class="box"><a href="'+ img + '" title="" data-toggle="lightbox" data-gallery="portfolio" data-title="Portfolio image 1" data-footer="Some footer information"><img src="'+ img + '" alt="" class="img-responsive"></a></div>')
+                $(".portfolio-images").append('<div class="col-sm-4"><div class="box"><a href="'+ img + '" title="" data-toggle="lightbox" data-gallery="portfolio" data-title="Portfolio image ' + count + '" data-footer="Some footer information"><img src="'+ img + '" alt="" class="img-responsive"></a></div>')
+                count ++
             });
         }
     });
