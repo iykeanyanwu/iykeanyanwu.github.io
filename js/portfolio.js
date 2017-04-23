a493d5f75754e1ff220ba7500238d8af1b98ba65
Kosy Anyanwu <kosyfrances@gmail.com>
2017-04-23 22:34:56 +0300

    Add portfolio pagination

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,17 +1,29 @@
-// This gets the images from images.json file and automatically
-// renders them on the DOM.
-
-$(function () {
+$(function() {
     var count = 1
 
+    // jPages customisation
+    function paginate() {
+        $("div.holder").jPages({
+            containerID: "itemContainer",
+            perPage: 9,
+            startPage: 1,
+            startRange: 1,
+            midRange: 5,
+            endRange: 1
+        });
+    }
+
+    // This gets the images from images.json file and automatically
+    // renders them on the DOM.
     $.ajax({
         url: "./images.json",
         dataType: "json",
         success: function(data) {
             $.each(data, function(key, img) {
-                $(".portfolio-images").append('<div class="col-sm-4"><div class="box"><a href="'+ img + '" title="" data-toggle="lightbox" data-gallery="portfolio" data-title="Portfolio image ' + count + '" data-footer="Some footer information"><img src="'+ img + '" alt="" class="img-responsive"></a></div>')
-                count ++
+                $(".portfolio-images").append('<div class="col-sm-4"><div class="box"><a href="' + img + '" title="" data-toggle="lightbox" data-gallery="portfolio" data-title="Portfolio image ' + count + '" data-footer="Some footer information"><img src="' + img + '" alt="" class="img-responsive"></a></div>')
+                count++
             });
-        }
+        },
+        complete: paginate,
     });
 });
